345ee0251e403fad57a5f178926f0662457ba5e9
anupamjhabbl <anupamimpbbl@gmail.com>
2023-05-17 20:56:12 +0530

    Added express validator for validation of values

--- inotebook-backend/routes/auth.js
+++ inotebook-backend/routes/auth.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const User = require('../models/User');
+const {body, validationResult} = require('express-validator');
 const router = express.Router();
 
-router.post('/',(req,res)=>{
-    console.log(req.body);
-    const user = User(req.body);
-    user.save();
-    res.send("it will listen a post request on /api/auth/ and create user of that and it don't require authnetication")
+router.post('/createUser',[
+    body('name',"Enter valid name").isLength({min:3}),
+    body('email',"Invalid email").isEmail(),
+    body('password',"Password must of length greater than or equal to 8").isLength({min:8})
+],async (req,res)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    
+    let user = await User.findOne({email:req.body.email});
+    if (user){
+      return res.status(400).json({"error":"This email is already registered"});
+    }
+    
+    user = await User.create({
+      name: req.body.name,
+      password: req.body.password,
+      email:req.body.email
+    });
+
+    return res.json(user);
 });
 
 module.exports = router;
--- inotebook-backend/routes/notes.js
+++ inotebook-backend/routes/notes.js
@@ -1,14 +1,24 @@
 const express = require('express');
-
+const {body, validationResult} = require('express-validator');
+const Notes = require('../models/Notes')
 const router = express.Router();
 
-router.get('/',(req,res)=>{
-    let obj = {
-        a:1,
-        b:2,
-        c:3
+router.post('/',[
+    body('title',"Title must be smaller than 50 letters").isLength({max:50}),
+    body('description',"Description must be greater than 100 letters").isLength({min:100})
+],(req,res)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-    res.json(obj);
+
+      Notes.create({
+      title: req.body.title,
+      description: req.body.description,
+      tag:req.body.tag
+    }).then(notes => res.json(notes)).catch(err => {
+        res.json({"error":"Please give unique title","message":err});
+    });
 });
 
 module.exports = router;
