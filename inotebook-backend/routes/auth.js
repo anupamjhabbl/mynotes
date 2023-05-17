ec8ba94e8cc32813b541a297f7e2f6f0a6d1205e
anupamjhabbl <anupamimpbbl@gmail.com>
2023-05-18 00:10:58 +0530

    Applied salting and hashing on password and token creation and sending to user

--- inotebook-backend/routes/auth.js
+++ inotebook-backend/routes/auth.js
@@ -1,30 +1,52 @@
 const express = require('express');
 const User = require('../models/User');
-const {body, validationResult} = require('express-validator');
+const { body, validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 const router = express.Router();
 
-router.post('/createUser',[
-    body('name',"Enter valid name").isLength({min:3}),
-    body('email',"Invalid email").isEmail(),
-    body('password',"Password must of length greater than or equal to 8").isLength({min:8})
-],async (req,res)=>{
+router.post('/createUser', [
+  body('name', "Enter valid name").isLength({ min: 3 }),            // validating 
+  body('email', "Invalid email").isEmail(),
+  body('password', "Password must of length greater than or equal to 8").isLength({ min: 8 })
+], async (req, res) => {
+  try {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    
-    let user = await User.findOne({email:req.body.email});
-    if (user){
-      return res.status(400).json({"error":"This email is already registered"});
+
+    // checking email is already registered or not
+    let user = await User.findOne({ email: req.body.email });
+    if (user) {
+      return res.status(400).json({ "error": "This email is already registered" });
     }
-    
+
+    // creating hash for the password
+    const salt = await bcrypt.genSalt(10);
+    const secPassword = await bcrypt.hash(req.body.password,salt);
+
+    // creating user
     user = await User.create({
       name: req.body.name,
-      password: req.body.password,
-      email:req.body.email
+      password: secPassword,
+      email: req.body.email
     });
 
-    return res.json(user);
+    const secret_key = "Anupam loves mustang, supra, GTR, camaro";
+    const data = {
+      user:{
+        id:user.id
+      }
+    }
+
+    // sending token to user
+    const authToken = jwt.sign(data,secret_key);
+    res.json(authToken);
+  }
+  catch (error) {
+    return res.status(500).send("Some error occured");
+  }
 });
 
 module.exports = router;
