e4ca35ae8a9bb1f77da83f766f055ee9638f3687
anupamjhabbl <anupamimpbbl@gmail.com>
2023-05-18 19:16:50 +0530

    added route for update and delete note

--- inotebook-backend/routes/notes.js
+++ inotebook-backend/routes/notes.js
@@ -30,15 +30,59 @@ router.post('/addNote', fetchUser, [
 
 // Router2 fetching all the notes: login required
 router.get('/fetchNotes', fetchUser, async (req, res) => {
-  try{
+  try {
     const notes = await Notes.find({ userId: req.user.id });
     res.json({ "notes": notes });
   }
-  catch(error){
+  catch (error) {
     res.status(500).send("Some Internal server error occured")
   }
 })
 
+// Route3 updating the notes : login required
+router.put('/updateNote/:id', fetchUser, async (req, res) => {
+  const { title, description, tag } = req.body;
+  let newNote = {};
+  if (title) { newNote.title = title };
+  if (description) { newNote.description = description };
+  if (tag) { newNote.tag = tag };
 
+  try {
+    const note = await Notes.findById(req.params.id);
+    if (!note) {
+      return res.status(404).send("Not Found");
+    }
+
+    if (note.userId.toString() !== req.user.id) {
+      return res.status(401).send("Access denied");
+    }
+
+    const updatedNote = await Notes.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true });
+    res.json({ "updatedNote": updatedNote });
+  }
+  catch (error) {
+    res.status(500).send("Internal Server error");
+  }
+})
+
+// Route4 : Deleting a node :login required
+router.delete('/deleteNote/:id', fetchUser, async (req, res) => {
+  try {
+    const note = await Notes.findById(req.params.id);
+    if (!note) {
+      return res.status(404).send("Not Found");
+    }
+
+    if (note.userId.toString() !== req.user.id) {
+      return res.status(401).send("Access Denied");
+    }
+
+    const deletedNote = await Notes.findByIdAndDelete(req.params.id);
+    res.json({ "deletedNote": deletedNote, "message": "Deleted Successfully" });
+  }
+  catch (error) {
+    res.status(500).send("Internal Server Error");
+  }
+})
 
 module.exports = router;
