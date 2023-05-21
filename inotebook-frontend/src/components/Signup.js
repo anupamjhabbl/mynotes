0ada89052e143067d2e14d5c6d89eb13ed9c6fed
anupamjhabbl <anupamimpbbl@gmail.com>
2023-05-21 11:12:46 +0530

    done some left thing

--- inotebook-frontend/src/components/Signup.js
+++ inotebook-frontend/src/components/Signup.js
@@ -1,54 +1,63 @@
 import { useContext, useState } from "react";
 import NoteContext from "../context/notes/NoteContext";
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const Signup = () => {
-    const [value, setValue] = useState({"name":"","email":"","password":""});
+    const [value, setValue] = useState({ "name": "", "email": "", "password": "" });
     const contextObj = useContext(NoteContext);
     let navigate = useNavigate();
 
     const onChangeMethod = (e) => {
-        setValue({...value, [e.target.name]:e.target.value});
+        setValue({ ...value, [e.target.name]: e.target.value });
     }
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const response = await fetch(`http://localhost:5000/api/auth/createUser`, {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json"
-            },
-            body: JSON.stringify(value),
-          });
-          const mainResponse = await response.json();
-          if (mainResponse.success){
-            contextObj.setSuccess(true);
-            localStorage.setItem("token",mainResponse.authToken);
-            navigate('/');
-          }
+        try {
+            const response = await fetch(`http://localhost:5000/api/auth/createUser`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(value),
+            });
+            const mainResponse = await response.json();
+            if (mainResponse.success) {
+                contextObj.setSuccess(true);
+                localStorage.setItem("token", mainResponse.authToken);
+                navigate('/');
+                contextObj.setAlertMethod("Successfully created the user");
+            }
+            else {
+                contextObj.setAlertMethod("Some error ocuured");
+            }
+        }
+        catch (error) {
+            contextObj.setAlertMethod("Some error ocuured");
+        }
         //   console.log(mainResponse);
-        contextObj.setAlertMethod("Successfully created the user");
+
     }
 
     return (
-        <div className="container w-50" style={{"marginTop":"130px"}}>
+        <div className="container w-50" style={{ "marginTop": "130px" }}>
             <h1 className="text-center my-5">SignUp</h1>
             <form className="my-5" onSubmit={handleSubmit}>
                 <div className="mb-3">
                     <label htmlFor="exampleInputEmail1" className="form-label">Name</label>
-                    <input type="text" className="form-control" id="name" value={value.name} name="name" aria-describedby="emailHelp" onChange={onChangeMethod}/>
+                    <input type="text" className="form-control" id="name" value={value.name} name="name" aria-describedby="emailHelp" onChange={onChangeMethod} required />
                     <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                    <input type="email" className="form-control" id="email" value={value.email} name="email" aria-describedby="emailHelp" onChange={onChangeMethod}/>
+                    <input type="email" className="form-control" id="email" value={value.email} name="email" aria-describedby="emailHelp" onChange={onChangeMethod} required />
                     <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="password" name="password" value={value.password} onChange={onChangeMethod}/>
+                    <input type="password" className="form-control" id="password" name="password" value={value.password} onChange={onChangeMethod} required />
                 </div>
-                <button type="submit" className={`btn btn-primary ${value.password.length<=8 ? "disabled" : ""}`}>Submit</button>
+                <button type="submit" className={`btn btn-primary ${value.password.length <= 8 ? "disabled" : ""}`}>Submit</button>
             </form>
         </div>
     )
